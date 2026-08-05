b2500095851d51987cd1f1c9726cc7b7de5c69c2
Priyanshu Singh <102438613+Commandarprime@users.noreply.github.com>
2026-08-05 23:23:55 +0530

    Time: 63 ms (75.96%), Space: 283.8 MB (61.54%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        for (int[] inv : invocations) {
+            graph.get(inv[0]).add(inv[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(k);
+        suspicious[k] = true;
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int next : graph.get(curr)) {
+                if (!suspicious[next]) {
+                    suspicious[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+
+        for (int[] inv : invocations) {
+            int a = inv[0], b = inv[1];
+            if (suspicious[b] && !suspicious[a]) {
+                List<Integer> all = new ArrayList<>();
+                for (int i = 0; i < n; i++) all.add(i);
+                return all;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) result.add(i);
+        }
+        return result;
+    }
+}
